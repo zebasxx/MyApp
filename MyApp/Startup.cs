702c54f6168946001ad271a578e87de91c443430
zebasxx <zebasxx@hotmail.com>
2018-07-31 12:39:22 -0300

    Chaanged Startup.cs

--- MyApp/Startup.cs
+++ MyApp/Startup.cs
@@ -27,7 +27,7 @@ namespace MyApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Pepe Escopeta!!!!");
             });
         }
     }
